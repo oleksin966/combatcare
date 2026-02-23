e41741f3a1f0c788d3bce41cfbebdcc0eb7c84ac
oleksin966 <oleksin966@gmail.com>
2026-02-23 16:46:38 +0300

    Initial commit

---
+++ accounts/permissions.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import Permission
+
+def get_volunteer_permissions():
+    return [
+        Permission.objects.get(codename='add_category'),
+        Permission.objects.get(codename='change_category'),
+        Permission.objects.get(codename='delete_category'),
+        Permission.objects.get(codename='add_subcategory'),
+        Permission.objects.get(codename='change_subcategory'),
+        Permission.objects.get(codename='delete_subcategory'),
+        Permission.objects.get(codename='add_item'),
+        Permission.objects.get(codename='change_item'),
+        Permission.objects.get(codename='delete_item'),
+        Permission.objects.get(codename='view_military'),
+        Permission.objects.get(codename='view_volunteer'),
+        Permission.objects.get(codename='view_deliveryman'),
+    ]
---
+++ chat/consumers.py
@@ -0,0 +1,84 @@
+import json
+from hashlib import md5
+from channels.generic.websocket import WebsocketConsumer
+from asgiref.sync import async_to_sync
+from accounts.models import CustomUser
+from .models import Message, Chat
+from datetime import datetime
+
+
+class ChatConsumer(WebsocketConsumer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(args, kwargs)
+        self.room_group_name = None
+
+    def connect(self):
+        sender_username = self.scope["user"].username
+        recipient_username = self.scope["url_route"]["kwargs"]["room_name"]
+        usernames = sorted([sender_username, recipient_username])
+
+        room_hash = md5(f"{usernames[0]}_{usernames[1]}".encode('utf-8')).hexdigest()
+        self.room_group_name = f"chat_{room_hash}"
+
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name,
+            self.channel_name
+        )
+
+        self.accept()
+
+
+    def disconnect(self, close_code):
+        # Leave room group
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name, self.channel_name
+        )
+
+    def receive(self, text_data: str | None) -> None:
+        text_data_json = json.loads(text_data)
+        message = text_data_json.get("message", "")
+        author = text_data_json.get("author", self.scope["user"].username)
+        recipient_username = self.scope["url_route"]["kwargs"]["room_name"]
+
+        chat = Chat.objects.filter(users=self.scope['user']).filter(users__username=recipient_username).first()
+        if not chat:
+            chat = Chat.objects.create()
+            chat.users.add(self.scope['user'], CustomUser.objects.get(username=recipient_username))
+
+        if message:
+            message_ = Message.objects.create(
+                chat=chat,
+                sender=self.scope['user'],
+                recipient=CustomUser.objects.get(username=recipient_username),
+                text=message,
+                timestamp=datetime.now()
+            )
+
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name, {"type": "chat_message", "message": message, "author": author}
+        )
+
+
+    # Receive message from room group
+    def chat_message(self, event):
+        message = event["message"]
+        message_type = event["type"]
+        author = event["author"]
+
+        self.send(text_data=json.dumps({"message": message, "type": message_type, "author": author}))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
